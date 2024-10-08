fda1aad6fec248723c910ff7773c94ed6fe7ba67
Paulo Sousa <paulopgss@gmail.com>
2024-10-08 10:01:42 -0300

    Add health check endpoint to handlers package

---
+++ internal/handlers/health.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func HealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "Server is running",
+	})
+}
